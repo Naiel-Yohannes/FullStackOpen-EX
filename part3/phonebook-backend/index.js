f5aab06f6d5567a4b01049543db1384096eaa2fd
Naiel Yohannes <naielyohannes@gmail.com>
2026-01-29 10:49:13 -0800

    adding lint to make the code consistant and so that to avoid future bugs

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,19 +6,19 @@ const Person = require('./modules/persons')
 const app = express()
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 morgan.token('body', req => {
-    return JSON.stringify(req.body)
+  return JSON.stringify(req.body)
 })
 
 app.use(express.static('dist'))
@@ -27,78 +27,78 @@ app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(errorHandler)
 
-const path = require('path');
+const path = require('path')
 
 app.get('/api/persons', (req, res, next) => {
-    Person.find({}).then(allPeople => {
-        res.json(allPeople)
-    })
+  Person.find({}).then(allPeople => {
+    res.json(allPeople)
+  })
     .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    Person.findById(id).then(person => {
-        if(person){
-            res.json(person)
-        }else{
-            res.status(404).end()
-        }    
-    })
+  const id = req.params.id
+  Person.findById(id).then(person => {
+    if(person){
+      res.json(person)
+    }else{
+      res.status(404).end()
+    }
+  })
     .catch(error => next(error))
 })
 
-app.get('/info', (req, res, next) => {
-    const d = new Date()
-    Person.countDocuments({}).then(count => {
-        res.send(`<div>
+app.get('/info', (req, res) => {
+  const d = new Date()
+  Person.countDocuments({}).then(count => {
+    res.send(`<div>
             <p>phonebook has info for ${count} people</p>
             <p>${d}</p>
         </div>`)
-    })
-    
+  })
+
 })
 
 app.post('/api/persons', async (req, res, next) => {
-    const body = req.body
-    const exists = await Person.findOne({name: body.name})
-    if(exists){
-        res.status(409).json({error: 'name already exists'})
-    } else if(body.name && body.number){
-        const person = new Person ({
-            name: body.name,
-            number: body.number
-        })
-        person.save().then(savedPerson => {
-            res.json(savedPerson)
-        })
-        .catch(error => next(error))
-    } else {
-        res.status(400).json({error: 'content missing'})
-    }
+  const body = req.body
+  const exists = await Person.findOne({ name: body.name })
+  if(exists){
+    res.status(409).json({ error: 'name already exists' })
+  } else if(body.name && body.number){
+    const person = new Person ({
+      name: body.name,
+      number: body.number
+    })
+    person.save().then(savedPerson => {
+      res.json(savedPerson)
+    })
+      .catch(error => next(error))
+  } else {
+    res.status(400).json({ error: 'content missing' })
+  }
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const {name, number} = req.body
+  const { name, number } = req.body
 
-    Person.findById(req.params.id).then(person => {
-        if(!person){
-            return res.status(404).end()
-        }
-        if (name !== undefined) person.name = name
-        if (number !== undefined) person.number = number
+  Person.findById(req.params.id).then(person => {
+    if(!person){
+      return res.status(404).end()
+    }
+    if (name !== undefined) person.name = name
+    if (number !== undefined) person.number = number
 
-        return person.save().then(person => {
-            res.json(person)
-        })
-        .catch(error => next(error))
+    return person.save().then(person => {
+      res.json(person)
     })
+      .catch(error => next(error))
+  })
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    Person.findByIdAndDelete(id).then(() => res.status(204).end())
+  const id = req.params.id
+  Person.findByIdAndDelete(id).then(() => res.status(204).end())
     .catch(error => next(error))
 })
 
@@ -107,7 +107,7 @@ app.use((req, res) => {
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
--- part3/phonebook-backend/modules/persons.js
+++ part3/phonebook-backend/modules/persons.js
@@ -11,37 +11,37 @@ if (!url) {
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url, { family: 4 })
-.then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
-})
-.catch(error => {
+  })
+  .catch(error => {
     console.log('error connecting to MongoDB:', error.message)
-})
+  })
 
 const personsSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: function(v){
-                return /\d{2,3}-d+/.test(v)
-            }, 
-            message: props => `${props.value} is not a vaalid number`
-        }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v){
+        return /\d{2,3}-d+/.test(v)
+      },
+      message: props => `${props.value} is not a vaalid number`
     }
+  }
 })
 
 personsSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personsSchema)
