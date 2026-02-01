c673ef2dc192301847af5ab60109598e68dce93d
Naiel Yohannes <naielyohannes@gmail.com>
2026-02-01 01:34:07 -0800

    Ex 4.4 solution

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogPosts) => {
+  const sum = blogPosts.reduce((acc, val) => {return acc + val.likes}, 0)
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
